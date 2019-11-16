5883337407db8e029c71af9fb873c6d2dba4a2a8
team <fake_email@notanaddress.edu>
2019-11-16 15:10:14 +0000

    Flask server updates

--- flask-server/flaskportal.py
+++ flask-server/flaskportal.py
@@ -1,6 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/input")
+def input():
+    return render_template("input.html")
+
+@app.route("/charts")
+def charts():
+    return render_template("charts.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port='5000')
