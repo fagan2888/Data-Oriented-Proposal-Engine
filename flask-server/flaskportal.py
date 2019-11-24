11012fb3595b9d3618eceafe429a67b129c66fba
team <fake_email@notanaddress.edu>
2019-11-24 21:57:56 +0000

    Flask updates

--- flask-server/flaskportal.py
+++ flask-server/flaskportal.py
@@ -18,4 +18,4 @@ def charts():
     return render_template("charts.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='8090')
+    app.run(debug=True, host='0.0.0.0', port='8090', threaded=True)
